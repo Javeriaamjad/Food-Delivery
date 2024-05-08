af8954d8b8b2a13774cf27e6d8fc2e365c1e7f09
Javeriaamjad <javeriaamjad37@gmail.com>
2024-05-09 00:19:50 +0500

    Add new files and directories

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className="Header">
+    <div className="header">
         <div className="header-contents">
             <h2>Order Your Favourite Food Here</h2>
             <p>choose a diverse menu featuring a delectabl array of  dishes crafed with the finest igredients and satisfy your carivngs and elevate your dining exprerience , one deliciuous meal at a time  </p>
