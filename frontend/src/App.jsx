2c78554cc8255cedb7adb31c203deeea4b92d530
Javeriaamjad <javeriaamjad37@gmail.com>
2024-06-11 18:28:19 +0500

    Home Page Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
+import Footer from "./components/Footer/Footer";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
+    <>
     <div className="app">
       <Navbar />
       <Routes>
@@ -15,6 +17,8 @@ const App = () => {
         <Route path="/order" element={<PlaceOrder />} />
       </Routes>
     </div>
+    <Footer></Footer>
+    </>
   );
 };
 
--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,16 +10,19 @@ const FoodDisplay = ({ category }) => {
       <h2 className="">Top Dishes Near You</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={(item._id)}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            ></FoodItem>
-          );
+          if(category==="All" || category === item.category){
+            return (
+              <FoodItem
+                key={index}
+                id={(item._id)}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              ></FoodItem>
+            );
+          }
+          
         })}
       </div>
     </div>
